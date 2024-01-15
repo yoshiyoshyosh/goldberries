74daddf043fd1283e4b8f7fcf327e00347f605a4
Hannes Ru <viddie@hotmail.de>
2024-01-15 13:22:21 +0100

    Added new concept of api to object classes and api/golden-list

--- include_bootstrap/objects/map.php
+++ include_bootstrap/objects/map.php
@@ -32,30 +32,30 @@ class Map
     return true;
   }
 
-  function apply_db_data($arr)
+  function apply_db_data($arr, $prefix = '')
   {
-    $this->id = intval($arr['id']);
-    $this->name = $arr['name'];
-    $this->is_rejected = $arr['is_rejected'] === 't';
-    $this->is_archived = $arr['is_archived'] === 't';
-    $this->date_added = $arr['date_added'];
+    $this->id = intval($arr[$prefix . 'id']);
+    $this->name = $arr[$prefix . 'name'];
+    $this->is_rejected = $arr[$prefix . 'is_rejected'] === 't';
+    $this->is_archived = $arr[$prefix . 'is_archived'] === 't';
+    $this->date_added = $arr[$prefix . 'date_added'];
 
-    if (isset($arr['url']))
-      $this->url = $arr['url'];
-    if (isset($arr['side']))
-      $this->side = $arr['side'];
-    if (isset($arr['rejection_reason']))
-      $this->rejection_reason = $arr['rejection_reason'];
-    if (isset($arr['campaign_id']))
-      $this->campaign_id = intval($arr['campaign_id']);
-    if (isset($arr['sort_major']))
-      $this->sort_major = intval($arr['sort_major']);
-    if (isset($arr['sort_minor']))
-      $this->sort_minor = intval($arr['sort_minor']);
-    if (isset($arr['sort_order']))
-      $this->sort_order = intval($arr['sort_order']);
-    if (isset($arr['authors']))
-      $this->authors = explode("\t", $arr['authors']);
+    if (isset($arr[$prefix . 'url']))
+      $this->url = $arr[$prefix . 'url'];
+    if (isset($arr[$prefix . 'side']))
+      $this->side = $arr[$prefix . 'side'];
+    if (isset($arr[$prefix . 'rejection_reason']))
+      $this->rejection_reason = $arr[$prefix . 'rejection_reason'];
+    if (isset($arr[$prefix . 'campaign_id']))
+      $this->campaign_id = intval($arr[$prefix . 'campaign_id']);
+    if (isset($arr[$prefix . 'sort_major']))
+      $this->sort_major = intval($arr[$prefix . 'sort_major']);
+    if (isset($arr[$prefix . 'sort_minor']))
+      $this->sort_minor = intval($arr[$prefix . 'sort_minor']);
+    if (isset($arr[$prefix . 'sort_order']))
+      $this->sort_order = intval($arr[$prefix . 'sort_order']);
+    if (isset($arr[$prefix . 'authors']))
+      $this->authors = explode("\t", $arr[$prefix . 'authors']);
   }
 
   function clone_for_api($DB)
