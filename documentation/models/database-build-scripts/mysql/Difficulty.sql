45178b75b76457d0cec9f9f3fd516195004e6092
Hannes Ru <viddie@hotmail.de>
2024-01-15 20:05:59 +0100

    Updated Difficulty and Player in model

--- documentation/models/database-build-scripts/mysql/Difficulty.sql
+++ documentation/models/database-build-scripts/mysql/Difficulty.sql
@@ -6,12 +6,10 @@ DROP TABLE IF EXISTS `Difficulty`;
 
 CREATE TABLE IF NOT EXISTS `Difficulty`
 (
- `id`          int NOT NULL AUTO_INCREMENT ,
- `name`        varchar(32) NOT NULL ,
- `subtier`     enum('high', 'mid', 'low', 'guard') NULL ,
- `sort`        int NOT NULL ,
- `color`       varchar(32) NULL ,
- `color_group` varchar(32) NULL ,
+ `id`      int NOT NULL AUTO_INCREMENT ,
+ `name`    varchar(32) NOT NULL ,
+ `subtier` enum('high', 'mid', 'low', 'guard') NULL ,
+ `sort`    int NOT NULL ,
 
 PRIMARY KEY (`id`)
 );
