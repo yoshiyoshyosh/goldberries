785b445d3e3d99442e50e24ce56196984e77923e
Hannes Ru <viddie@hotmail.de>
2024-01-15 13:22:41 +0100

    Added back accidentally deleted fields in Difficulty

--- documentation/models/database-build-scripts/mysql/Difficulty.sql
+++ documentation/models/database-build-scripts/mysql/Difficulty.sql
@@ -6,10 +6,12 @@ DROP TABLE IF EXISTS `Difficulty`;
 
 CREATE TABLE IF NOT EXISTS `Difficulty`
 (
- `id`      int NOT NULL AUTO_INCREMENT ,
- `name`    varchar(32) NOT NULL ,
- `subtier` enum('high', 'mid', 'low', 'guard') NULL ,
- `sort`    int NOT NULL ,
+ `id`          int NOT NULL AUTO_INCREMENT ,
+ `name`        varchar(32) NOT NULL ,
+ `subtier`     enum('high', 'mid', 'low', 'guard') NULL ,
+ `sort`        int NOT NULL ,
+ `color`       varchar(32) NULL ,
+ `color_group` varchar(32) NULL ,
 
 PRIMARY KEY (`id`)
 );
--- documentation/models/database-build-scripts/mysql/goldberries.sql
+++ documentation/models/database-build-scripts/mysql/goldberries.sql
@@ -50,10 +50,12 @@ PRIMARY KEY (`id`)
 -- ====== Difficulty ======
 CREATE TABLE IF NOT EXISTS `Difficulty`
 (
- `id`      int NOT NULL AUTO_INCREMENT ,
- `name`    varchar(32) NOT NULL ,
- `subtier` enum('high', 'mid', 'low', 'guard') NULL ,
- `sort`    int NOT NULL ,
+ `id`          int NOT NULL AUTO_INCREMENT ,
+ `name`        varchar(32) NOT NULL ,
+ `subtier`     enum('high', 'mid', 'low', 'guard') NULL ,
+ `sort`        int NOT NULL ,
+ `color`       varchar(32) NULL ,
+ `color_group` varchar(32) NULL ,
 PRIMARY KEY (`id`)
 );
 
