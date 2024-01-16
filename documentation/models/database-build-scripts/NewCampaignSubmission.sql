9e67f42dcf5ec17a6fbb9b46385714d46954a396
yosh <yosh-git@riseup.net>
2024-01-16 12:08:34 -0500

    update database scripts for the hopefully final time

--- documentation/models/database-build-scripts/NewCampaignSubmission.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS NewCampaignSubmission;
-
--- ====== NewCampaignSubmission ======
-CREATE TABLE IF NOT EXISTS NewCampaignSubmission
-(
-	id          SERIAL PRIMARY KEY,
-	url         varchar(256) NOT NULL,
-	description text NOT NULL
-);
