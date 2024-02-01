2bda8733a940f713b9ca1fdea2f3ed929471d308
Hannes Ru <viddie@hotmail.de>
2024-02-01 18:11:15 +0100

    Added paginated challenge GET endpoint, Added views to DB model

--- api/challenge.php
+++ api/challenge.php
@@ -3,29 +3,92 @@
 require_once('api_bootstrap.inc.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-  $id = $_REQUEST['id'];
   $submissions = isset($_REQUEST['submissions']) && $_REQUEST['submissions'] === 'true';
-  $is_full_game = isset($_REQUEST['is_full_game']) && $_REQUEST['is_full_game'] === 'true';
   $depth = isset($_REQUEST['depth']) ? intval($_REQUEST['depth']) : 3;
 
-  $challenges = Challenge::get_request($DB, $id);
-  if ($submissions) {
+  if (isset($_REQUEST['id'])) {
+    $id = $_REQUEST['id'];
+    $challenges = Challenge::get_request($DB, $id);
+    if ($submissions) {
+      if (is_array($challenges)) {
+        foreach ($challenges as $challenge) {
+          $challenge->fetch_submissions($DB);
+        }
+      } else {
+        $challenges->fetch_submissions($DB);
+      }
+    }
+
     if (is_array($challenges)) {
       foreach ($challenges as $challenge) {
-        $challenge->fetch_submissions($DB);
+        $challenge->expand_foreign_keys($DB, $depth);
       }
     } else {
-      $challenges->fetch_submissions($DB);
+      $challenges->expand_foreign_keys($DB, $depth);
     }
-  }
 
-  if (is_array($challenges)) {
-    foreach ($challenges as $challenge) {
-      $challenge->expand_foreign_keys($DB, $depth);
+    api_write($challenges);
+
+  } else if (isset($_REQUEST['page'])) {
+    $page = intval($_REQUEST['page']) ?? 1;
+    $per_page = intval($_REQUEST['per_page']) ?? 50;
+    $search = isset($_REQUEST['search']) ? $_REQUEST['search'] : null;
+    $sort = isset($_REQUEST['sort']) ? $_REQUEST['sort'] : null;
+    $sort_dir = isset($_REQUEST['sort_dir']) ? $_REQUEST['sort_dir'] : null;
+
+    $query = "SELECT * FROM view_challenges";
+
+    if ($search !== null) {
+      $search = pg_escape_string($search);
+      $query .= " WHERE campaign_name ILIKE '%" . $search . "%' OR map_name ILIKE '%" . $search . "%'";
     }
-  } else {
-    $challenges->expand_foreign_keys($DB, $depth);
-  }
 
-  api_write($challenges);
+    if ($sort !== null) {
+      $sort = pg_escape_string($sort);
+      $query .= " ORDER BY " . $sort;
+      if ($sort_dir !== null) {
+        $sort_dir = pg_escape_string($sort_dir);
+        $query .= " " . $sort_dir;
+      }
+    }
+
+    $query = "
+    WITH challenges AS (
+      " . $query . "
+    )
+    SELECT *, count(*) OVER () AS total_count
+    FROM challenges";
+
+    if ($per_page !== -1) {
+      $query .= " LIMIT " . $per_page . " OFFSET " . ($page - 1) * $per_page;
+    }
+
+    $result = pg_query($DB, $query);
+    if (!$result) {
+      die_json(500, "Failed to query database");
+    }
+
+    $maxCount = 0;
+    $challenges = array();
+    while ($row = pg_fetch_assoc($result)) {
+      $challenge = new Challenge();
+      $challenge->apply_db_data($row, "challenge_");
+      $challenge->expand_foreign_keys($row, $depth);
+      $challenges[] = $challenge;
+
+      if ($maxCount === 0) {
+        $maxCount = intval($row['total_count']);
+      }
+    }
+
+    api_write(
+      array(
+        'challenges' => $challenges,
+        'max_count' => $maxCount,
+        'max_page' => ceil($maxCount / $per_page),
+        'page' => $page,
+        'per_page' => $per_page,
+      )
+    );
+  }
 }
--- documentation/models/database-build-scripts/goldberries.sql
+++ documentation/models/database-build-scripts/goldberries.sql
@@ -219,3 +219,163 @@ CREATE TABLE fwg_data
  CONSTRAINT farewellgoldendata_pkey PRIMARY KEY ( "id" ),
  CONSTRAINT farewellgoldendata_submission_id_fkey FOREIGN KEY ( submission_id ) REFERENCES submission ( "id" ) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
+
+-- =========== VIEWS ===========
+CREATE VIEW "view_submissions" AS SELECT                                                                   
+  campaign.id AS campaign_id,                                            
+  campaign.name AS campaign_name,                                        
+  campaign.url AS campaign_url,                                          
+  campaign.date_added AS campaign_date_added,                            
+  campaign.icon_url AS campaign_icon_url,                                
+  campaign.sort_major_name AS campaign_sort_major_name,                  
+  campaign.sort_major_labels AS campaign_sort_major_labels,              
+  campaign.sort_major_colors AS campaign_sort_major_colors,
+  campaign.sort_minor_name AS campaign_sort_minor_name,                  
+  campaign.sort_minor_labels AS campaign_sort_minor_labels,              
+  campaign.sort_minor_colors AS campaign_sort_minor_colors,
+  campaign.author_gb_id AS campaign_author_gb_id,                        
+  campaign.author_gb_name AS campaign_author_gb_name,                    
+                                                                         
+  map.id AS map_id,                                                      
+  map.campaign_id AS map_campaign_id,                                    
+  map.name AS map_name,                                                  
+  map.url AS map_url,                                                    
+  map.date_added AS map_date_added,                                      
+  map.is_rejected AS map_is_rejected,                                    
+  map.rejection_reason AS map_rejection_reason,
+  map.is_archived AS map_is_archived,
+  map.has_fc AS map_has_fc,
+  map.sort_major AS map_sort_major,
+  map.sort_minor AS map_sort_minor,
+  map.sort_order AS map_sort_order,
+  map.author_gb_id AS map_author_gb_id,
+  map.author_gb_name AS map_author_gb_name,
+
+  challenge.id AS challenge_id,
+  challenge.campaign_id AS challenge_campaign_id,
+  challenge.map_id AS challenge_map_id,
+  challenge.objective_id AS challenge_objective_id,
+  challenge.description AS challenge_description,
+  challenge.difficulty_id AS challenge_difficulty_id,
+  challenge.date_created AS challenge_date_created,
+  challenge.requires_fc AS challenge_requires_fc,
+  challenge.has_fc AS challenge_has_fc,
+  challenge.is_arbitrary AS challenge_is_arbitrary,
+
+  cd.id AS difficulty_id,
+  cd.name AS difficulty_name,
+  cd.subtier AS difficulty_subtier,
+  cd.sort AS difficulty_sort,
+
+  objective.id AS objective_id,
+  objective.name AS objective_name,
+  objective.description AS objective_description,
+  objective.display_name_suffix AS objective_display_name_suffix,
+  objective.is_arbitrary AS objective_is_arbitrary,
+
+  submission.id AS submission_id,
+  submission.challenge_id AS submission_challenge_id,
+  submission.player_id AS submission_player_id,
+  submission.date_created AS submission_date_created,
+  submission.is_fc AS submission_is_fc,
+  submission.proof_url AS submission_proof_url,
+  submission.raw_session_url AS submission_raw_session_url,
+  submission.player_notes AS submission_player_notes,
+  submission.suggested_difficulty_id AS submission_suggested_difficulty_id,
+  submission.is_verified AS submission_is_verified,
+  submission.is_rejected AS submission_is_rejected,
+  submission.date_verified AS submission_date_verified,
+  submission.verifier_notes AS submission_verifier_notes,
+  submission.verifier_id AS submission_verifier_id,
+  submission.new_challenge_id AS submission_new_challenge_id,
+
+  p.id AS player_id,
+  p.name AS player_name,
+
+  v.id AS verifier_id,
+  v.name AS verifier_name,
+
+  pd.id AS suggested_difficulty_id,
+  pd.name AS suggested_difficulty_name,
+  pd.subtier AS suggested_difficulty_subtier,
+  pd.sort AS suggested_difficulty_sort
+
+FROM campaign
+JOIN map  ON campaign.id = map.campaign_id
+JOIN challenge  ON map.id = challenge.map_id
+JOIN difficulty cd ON challenge.difficulty_id = cd.id
+JOIN objective  ON challenge.objective_id = objective.id
+JOIN submission  ON challenge.id = submission.challenge_id
+JOIN player p ON p.id = submission.player_id
+LEFT JOIN player v ON v.id = submission.verifier_id
+LEFT JOIN difficulty pd ON submission.suggested_difficulty_id = pd.id
+
+ORDER BY campaign.name, map.sort_major, map.sort_minor, map.sort_order, map.name, cd.sort, submission.id ;
+
+
+
+CREATE VIEW "view_challenges" AS SELECT                                                                   
+  campaign.id AS campaign_id,                                            
+  campaign.name AS campaign_name,                                        
+  campaign.url AS campaign_url,                                          
+  campaign.date_added AS campaign_date_added,                            
+  campaign.icon_url AS campaign_icon_url,                                
+  campaign.sort_major_name AS campaign_sort_major_name,                  
+  campaign.sort_major_labels AS campaign_sort_major_labels,              
+  campaign.sort_major_colors AS campaign_sort_major_colors,
+  campaign.sort_minor_name AS campaign_sort_minor_name,                  
+  campaign.sort_minor_labels AS campaign_sort_minor_labels,              
+  campaign.sort_minor_colors AS campaign_sort_minor_colors,
+  campaign.author_gb_id AS campaign_author_gb_id,                        
+  campaign.author_gb_name AS campaign_author_gb_name,                    
+                                                                         
+  map.id AS map_id,                                                      
+  map.campaign_id AS map_campaign_id,                                    
+  map.name AS map_name,                                                  
+  map.url AS map_url,                                                    
+  map.date_added AS map_date_added,                                      
+  map.is_rejected AS map_is_rejected,                                    
+  map.rejection_reason AS map_rejection_reason,
+  map.is_archived AS map_is_archived,
+  map.has_fc AS map_has_fc,
+  map.sort_major AS map_sort_major,
+  map.sort_minor AS map_sort_minor,
+  map.sort_order AS map_sort_order,
+  map.author_gb_id AS map_author_gb_id,
+  map.author_gb_name AS map_author_gb_name,
+
+  challenge.id AS challenge_id,
+  challenge.campaign_id AS challenge_campaign_id,
+  challenge.map_id AS challenge_map_id,
+  challenge.objective_id AS challenge_objective_id,
+  challenge.description AS challenge_description,
+  challenge.difficulty_id AS challenge_difficulty_id,
+  challenge.date_created AS challenge_date_created,
+  challenge.requires_fc AS challenge_requires_fc,
+  challenge.has_fc AS challenge_has_fc,
+  challenge.is_arbitrary AS challenge_is_arbitrary,
+
+  cd.id AS difficulty_id,
+  cd.name AS difficulty_name,
+  cd.subtier AS difficulty_subtier,
+  cd.sort AS difficulty_sort,
+
+  objective.id AS objective_id,
+  objective.name AS objective_name,
+  objective.description AS objective_description,
+  objective.display_name_suffix AS objective_display_name_suffix,
+  objective.is_arbitrary AS objective_is_arbitrary,
+
+  COUNT(submission.id) AS count_submissions
+
+FROM campaign
+JOIN map  ON campaign.id = map.campaign_id
+JOIN challenge  ON map.id = challenge.map_id
+JOIN difficulty cd ON challenge.difficulty_id = cd.id
+JOIN objective  ON challenge.objective_id = objective.id
+LEFT JOIN submission  ON challenge.id = submission.challenge_id
+
+GROUP BY campaign.id, map.id, challenge.id, cd.id, objective.id
+ORDER BY campaign.name, map.sort_major, map.sort_minor, map.sort_order, map.name, cd.sort ;
