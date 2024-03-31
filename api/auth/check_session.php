23f9344c5c1140b96aa29ee53b3b2301dca950e4
Hannes Ru <viddie@hotmail.de>
2024-03-31 13:36:46 +0200

    Added expansion on player in check_session

--- api/auth/check_session.php
+++ api/auth/check_session.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('../api_bootstrap.inc.php');
+require_once ('../api_bootstrap.inc.php');
 
 $account = get_user_data();
 
@@ -8,5 +8,6 @@ if ($account == null) {
   die_json(401, "Not logged in");
 }
 
+$account->expand_foreign_keys($DB, 3);
 $account->remove_sensitive_info();
 api_write($account);
--- include_bootstrap/objects/account.php
+++ include_bootstrap/objects/account.php
@@ -104,12 +104,14 @@ class Account extends DbObject
 
     if ($this->player_id !== null) {
       $this->player = Player::get_by_id($DB, $this->player_id, $depth - 1);
+      $this->player->expand_foreign_keys($DB, $depth - 1, false);
     } else {
       $this->player = null;
     }
 
     if ($this->claimed_player_id !== null) {
       $this->claimed_player = Player::get_by_id($DB, $this->claimed_player_id, $depth - 1);
+      $this->claimed_player->expand_foreign_keys($DB, $depth - 1, false);
     } else {
       $this->claimed_player = null;
     }
