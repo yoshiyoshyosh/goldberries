230ae72502ef775fffb3171d77bc7829b79d6b3f
Hannes Ru <viddie@hotmail.de>
2024-07-16 13:29:55 +0200

    More cleanup, Added port to DB connection string

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -4,7 +4,6 @@ define("GB_ROOT_LOCAL", dirname(__FILE__));
 
 require_once (GB_ROOT_LOCAL . "/include_bootstrap/config.php");
 require_once (GB_ROOT_LOCAL . "/include_bootstrap/DB.php");
-require_once (GB_ROOT_LOCAL . "/include_bootstrap/functions.php");
 
 $requireObjects = array(
   "dbobject",
--- include_bootstrap/config.php
+++ include_bootstrap/config.php
@@ -2,7 +2,7 @@
 
 DEFINE("ADMIN_EMAIL", "admin@domain.com");
 DEFINE("NOREPLY_EMAIL", "noreply@goldberries.net");
-DEFINE("DB_STRING", "host=localhost dbname=" . getenv("GB_DBNAME") . " user=" . getenv("GB_DBUSER") . " password=" . getenv("GB_DBPASS"));
+DEFINE("DB_STRING", "host=localhost port=" . getenv("GB_DBPORT") . " dbname=" . getenv("GB_DBNAME") . " user=" . getenv("GB_DBUSER") . " password=" . getenv("GB_DBPASS"));
 DEFINE("SUGGESTION_BOX_WEBHOOK_URL", getenv("SUGGESTION_BOX_WEBHOOK_URL"));
 DEFINE("PYTHON_COMMAND", getenv("PYTHON_COMMAND"));
 DEFINE("WKHTMLTOIMAGE_PATH", getenv("WKHTMLTOIMAGE_PATH")); //false if not set
