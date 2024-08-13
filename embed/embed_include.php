b6e020ed056c18d00a4d18ab74018b4ae6099e70
Hannes Ru <viddie@hotmail.de>
2024-08-13 21:03:49 +0200

    Fixed xss vulnerability with all text embeds

--- embed/embed_include.php
+++ embed/embed_include.php
@@ -3,19 +3,19 @@
 function output_text_embed($url, $title, $description, $site_name)
 {
   $template = file_get_contents("text-embed-template.html");
-  $template = str_replace("{{url}}", $url, $template);
-  $template = str_replace("{{title}}", $title, $template);
-  $template = str_replace("{{description}}", $description, $template);
-  $template = str_replace("{{site_name}}", $site_name, $template);
+  $template = str_replace("{{url}}", htmlspecialchars($url), $template);
+  $template = str_replace("{{title}}", htmlspecialchars($title), $template);
+  $template = str_replace("{{description}}", htmlspecialchars($description), $template);
+  $template = str_replace("{{site_name}}", htmlspecialchars($site_name), $template);
   echo $template;
 }
 
 function output_image_embed($url, $title, $description, $image_url)
 {
   $template = file_get_contents("image-embed-template.html");
-  $template = str_replace("{{url}}", $url, $template);
-  $template = str_replace("{{title}}", $title, $template);
-  $template = str_replace("{{description}}", $description, $template);
-  $template = str_replace("{{image_url}}", $image_url, $template);
+  $template = str_replace("{{url}}", htmlspecialchars($url), $template);
+  $template = str_replace("{{title}}", htmlspecialchars($title), $template);
+  $template = str_replace("{{description}}", htmlspecialchars($description), $template);
+  $template = str_replace("{{image_url}}", htmlspecialchars($image_url), $template);
   echo $template;
 }
