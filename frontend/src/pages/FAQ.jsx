6d7bd33003cdc82d0a807918d9f0d996650e6b81
Hannes Ru <viddie@hotmail.de>
2024-09-30 17:48:06 +0200

    Added vanilla references to FAQ as special entry

--- frontend/src/pages/FAQ.jsx
+++ frontend/src/pages/FAQ.jsx
@@ -1,9 +1,27 @@
-import { Paper, Stack, Typography } from "@mui/material";
-import { BasicContainerBox, HeadTitle, StyledLink } from "../components/BasicComponents";
-import { useTranslation } from "react-i18next";
+import {
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import {
+  BasicContainerBox,
+  ErrorDisplay,
+  HeadTitle,
+  LoadingSpinner,
+  StyledLink,
+} from "../components/BasicComponents";
+import { Trans, useTranslation } from "react-i18next";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { getQueryData, useGetAllDifficulties } from "../hooks/useApi";
+import { DifficultyChip } from "../components/GoldberriesComponents";
 
 export function PageFAQ() {
   const { t } = useTranslation(undefined, { keyPrefix: "faq" });
@@ -36,6 +54,7 @@ function FAQList() {
       {entries.map((entry, index) => (
         <FAQEntry key={index} entry={entry} />
       ))}
+      <FAQEntryVanillaReferences />
     </>
   );
 }
@@ -52,9 +71,75 @@ function FAQEntry({ entry }) {
         </Typography>
       </StyledLink>
       {expanded && (
-        <Typography variant="body1" sx={{ p: "1em" }}>
+        <Typography variant="body1" sx={{ p: "1em", paddingTop: 0 }}>
           {answer}
         </Typography>
+      )}
+    </Paper>
+  );
+}
+
+function FAQEntryVanillaReferences({}) {
+  const { t } = useTranslation(undefined, { keyPrefix: "faq.vanilla_references" });
+  const { t: t_g } = useTranslation(undefined, { keyPrefix: "general" });
+  const [expanded, setExpanded] = useState(false);
+  const query = useGetAllDifficulties();
+  const difficulties = getQueryData(query);
+
+  const vanilla_references = [
+    { name: "The Summit A-Side [FC]", difficulty_id: 17 },
+    { name: "Farewell [C/FC]", difficulty_id: 14 },
+    { name: "Farewell [No DTS] [C/FC]", difficulty_id: 12 },
+    { name: "Any%", difficulty_id: 14 },
+    { name: "All Full Clears", difficulty_id: 10 },
+    { name: "All B-Sides", difficulty_id: 10 },
+    { name: "All C-Sides", difficulty_id: 17 },
+    { name: "100%", difficulty_id: 4 },
+    { name: "202 Berries", difficulty_id: 3 },
+  ];
+
+  return (
+    <Paper style={{ marginBottom: "1em" }}>
+      <StyledLink onClick={() => setExpanded(!expanded)}>
+        <Typography variant="h5" sx={{ p: "0.5em" }}>
+          {t("question")} <FontAwesomeIcon icon={expanded ? faChevronUp : faChevronDown} size="2xs" />
+        </Typography>
+      </StyledLink>
+      {expanded && (
+        <Stack direction="column" gap={1} sx={{ p: "1em", paddingTop: 0 }}>
+          <Typography variant="body1">
+            <Trans>{t("answer")}</Trans>
+          </Typography>
+          {query.isLoading && <LoadingSpinner />}
+          {query.isError && <ErrorDisplay error={query.error} />}
+          {query.isSuccess && (
+            <Stack direction="row" justifyContent="space-around">
+              <Table size="small" sx={{ width: "unset" }}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell width={1}>{t_g("challenge", { count: 1 })}</TableCell>
+                    <TableCell align="center">{t_g("difficulty", { count: 1 })}</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {vanilla_references.map((ref, index) => {
+                    const difficulty = difficulties.find((d) => d.id === ref.difficulty_id);
+                    return (
+                      <TableRow key={index}>
+                        <TableCell width={1} sx={{ whiteSpace: "nowrap" }}>
+                          {ref.name}
+                        </TableCell>
+                        <TableCell align="center">
+                          <DifficultyChip difficulty={difficulty} />
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </Stack>
+          )}
+        </Stack>
       )}
     </Paper>
   );
